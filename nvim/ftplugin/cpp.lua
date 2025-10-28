05326c430aa9a08c6bfac4f38a53ba064341c32d
Dante999 <dante999@users.noreply.github.com>
2025-10-28 21:30:52 +0100

    Fix cpp files use c instead of cpp ft config

--- nvim/ftplugin/cpp.lua
+++ nvim/ftplugin/cpp.lua
@@ -1,8 +1,10 @@
+vim.b.did_ftplugin = true
+
 local opt = vim.opt_local
 
 opt.expandtab = true
 opt.tabstop = 4
-opt.softtabstop = 4
 opt.shiftwidth = 4
-----opt.colorcolumn = 121
+opt.softtabstop = 4
+--opt.colorcolumn = 81
 opt.textwidth = 120
